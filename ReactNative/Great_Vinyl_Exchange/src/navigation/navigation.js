48ccea34b4f2daea03eb45d627e7228b22813f12
Daniel Ley <danny.ley13@gmail.com>
2018-01-21 16:39:15 -0600

    US107: Login User Screen and Actions

--- ReactNative/Great_Vinyl_Exchange/src/navigation/navigation.js
+++ ReactNative/Great_Vinyl_Exchange/src/navigation/navigation.js
@@ -5,11 +5,13 @@ import AuthenticationMain from '../authentication/screens/AuthenticationMain';
 import AuthenticationSignUpEmail from '../authentication/screens/AuthenticationSignUpEmail';
 import AuthenticationSignUpPassword from '../authentication/screens/AuthenticationSignUpPassword';
 import AuthenticationSignUpConfirmPassword from '../authentication/screens/AuthenticationSignUpConfirmPassword';
+import AuthenicationSignIn from '../authentication/screens/AuthenticationSignIn';
 
 export default StackNavigator({
     AuthenticationMain: { screen: AuthenticationMain, navigationOptions: { header: null } },
     AuthenticationSignUpEmail: { screen: AuthenticationSignUpEmail, navigationOptions: { header: null } },
     AuthenticationSignUpPassword: { screen: AuthenticationSignUpPassword, navigationOptions: { header: null } },
-    AuthenticationSignUpConfirmPassword: { screen: AuthenticationSignUpConfirmPassword, navigationOptions: { header: null } }
+    AuthenticationSignUpConfirmPassword: { screen: AuthenticationSignUpConfirmPassword, navigationOptions: { header: null } },
+    AuthenicationSignIn: { screen: AuthenicationSignIn, navigationOptions: { header: null } }
 });
 
